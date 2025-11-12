2c379c14ebbdfd8aa3dfb2bf03961fc24540d407
islapatrick-cpu <islacpatrick@gmail.com>
2025-11-11 17:02:17 -0800

    nov 11

---
+++ exercises/lab7.js
@@ -0,0 +1,8 @@
+$(".nice-block span").click(function () {
+    $(this).parent().toggleClass("more");
+    if ($(this).html () =="more") {
+        $(this).html("less");
+    } else {
+        $(this).html("more");
+    }
+});
